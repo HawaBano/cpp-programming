7f680bac88f4886d425f6c2564af983f249c4a5a
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-11-28 19:40:43 +0500

    Add files via upload

---
+++ for loop 2 pattren.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char alpha='A';
+	int l,m,n;
+	cout<<"\n enter the value of n";
+	cin>>n;
+
+	for(l=0;l<=n;l++)
+	{
+		int j=0;
+		for(j=0;j<=l;j++)
+		{
+			cout<<alpha<<"\n ";
+			alpha++;
+		}
+	
+	}
+}

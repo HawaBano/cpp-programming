2303001ebf78eb5d33dd5ad3f75abe2ce5814b47
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-11-29 17:51:55 +0500

    Add files via upload

---
+++ cout statment in cpp program.cpp
@@ -0,0 +1,14 @@
+//program for country
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	int love;
+	int my;
+	int country;
+	int pakistan;
+	cout<<"i \n love \n my \n country \n pakistan"<<endl;
+	return 0;
+	
+}

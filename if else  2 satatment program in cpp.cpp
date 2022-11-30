94b5a8065665ed7b1cccfcbc4a12eb452d1213b6
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-11-30 17:49:16 +0500

    Add files via upload

---
+++ if else  2 satatment program in cpp.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int ang1,ang2,ang3;
+   cout<<"enter the angles of triangle"<<endl;
+   cin>>ang1,ang2,ang3;
+   if(ang1+ang2+ang3==180)
+   {
+   	cout<<"the angle are valid"<<endl;
+   	
+   }
+   else
+   {
+   	cout<<"the angle are not valid"<<endl;
+   }
+   
+
+	
+}

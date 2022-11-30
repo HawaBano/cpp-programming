13bec45a96730e717843f0d2ac805008b472c84f
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-11-30 23:11:45 +0500

    Add files via upload

---
+++ while loop.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=1;
+  while(a==1)
+  {
+  	int valueOne;
+  	cout<<"enter the valueOne"<<endl;
+  	cin>>valueOne;
+  	int valueTwo;
+  	cout<<"enter the valueTwo"<<endl;
+  	cin>>valueTwo;
+  	cout<<valueOne+valueTwo<<"the result is"<<endl;
+  	cout<<"press 1 to keep running and press any other key to stop running"<<endl;
+  	cin>>a;
+  	
+  }
+	cout<<"thank you for using this program";
+}

70bbc4a0d848a5be67698f09e7216794926e5b7e
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-11-30 17:50:07 +0500

    Add files via upload

---
+++ if else stament programm in cpp.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the value"<<endl;
+	cin>>a;
+	if(a%2==0)
+	{
+	
+	cout<<"even num"<<endl;
+    }
+	else
+	{
+	cout<<"odd num"<<endl;
+    }
+	
+}

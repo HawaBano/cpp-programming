b05df9ec4902cd0f96e5048e1431cf1c37587c56
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-12-06 16:41:55 +0500

    Add files via upload

---
+++ assignment in cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter the value of a"<<endl;
+	cin>>a;
+	while(a%2==!0)
+	{
+		cout<<"prime num"<<endl;
+		a++;
+		cout<<"press 1 to continue and press 2 to stop it"<<endl;	
+	}
+	    cout<<"thank you for using this prgramm"; 
+	
+	
+}

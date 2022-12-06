21c4c67751ceb06a1354f578be2dedc0fff1a684
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-12-06 18:02:19 +0500

    Add files via upload

---
+++ Arry program 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[4];
+	int i=0;
+	int number1;
+	int number2;
+	cout<<"enter number 1";
+	cin>>number1;
+	cout<<"enter number 2";
+	cin>>number2;
+	for(i=0;i<=4;i++)
+	{
+		cout<<"number 1 is"<<"number 2 is"<<endl;
+		cin>>a[i];
+	}
+	
+}

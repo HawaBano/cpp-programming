034eb79deed92d5af0b55e0f24b3aee40a6786c8
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-12-06 16:17:43 +0500

    Add files via upload

---
+++ odd num for loop into while loop in cpp.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i=0;
+	
+	for(i=0;i<=3;i++)
+	{
+		if(i%2==0)
+		{
+			cout<<"even num"<<endl;
+		}
+	int j=0;
+	while(j%2==0)
+	{
+		cout<<"even num"<<endl;
+		j++;
+		}	
+		
+	}
+
+}

8acd3345a5d1a2b6a7ee0d3d8d6835a4eeb37ccd
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-12-12 20:17:29 +0500

    Add files via upload

---
+++ do while loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+int main()
+{
+	
+	int a;
+	clrscr();
+	a=10;
+	do
+	{
+	cout<<a<<endl;
+	a=a-1;	
+	}
+	while(a>=1);
+	return 0;
+	
+}
---
+++ do while program.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b.c.d;
+	cout<<"enetr first number";
+	cin>>a;
+	cout<<"enetr second number";
+	cin>>b;
+	c=1;
+	d=1;
+	do
+	{
+		d=d*a;
+		c=c+1;
+		
+	}
+	while(c<=b);
+	cout<<"result is"<<d;
+	
+}

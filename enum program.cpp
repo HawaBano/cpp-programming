3bb72a40ddb9d654c609c080067988ea35da3b69
HawaBano <119333626+HawaBano@users.noreply.github.com>
2022-12-28 19:49:15 +0500

    Add files via upload

---
+++ enum program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+enum school{
+	classOne;
+	classTwo;
+	classThree;
+	classFour;
+	classFive;
+};
+int main()
+{
+	school c;
+	c=classThree;
+	cout<<"class is";
+	
+}
